ec06f4c0ed33755313823f15bf4a8c5bcf66b6b0
dianaboikoo <eaa23dibo@students.eaaa.dk>
2024-11-04 11:24:40 +0100

    base

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+
+import './App.css'
+import { Navigate, Route, Routes } from "react-router-dom";
+import AccountPage from "./pages/AccountPage";
+import CommunityPage from "./pages/CommunityPage";
+import HomePage from "./pages/HomePage";
+import StatisticsPage from "./pages/StatisticsPage";
+
+
+
+function App() {
+  return (
+    <>
+       <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/community" element={<CommunityPage />} />
+          <Route path="/statistics" element={<StatisticsPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </main>
+    </>
+  )
+}
+
+export default App
